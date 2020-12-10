90c7918845caaf32177ffe307fc462429432d7b9
Azzam Alsharafi <azzamalsharafi@gmail.com>
2020-12-10 14:14:26 +0300

    Day 7

---
+++ Day 7/7A.py
@@ -0,0 +1,36 @@
+import os.path
+
+rules = dict()
+
+gold = "shiny gold"
+none = "none"
+
+with open(os.path.join("Day 7", "input.txt")) as f:
+    for line in f:
+        bag, rule = line.split(" bags contain ")
+
+        if "no other" in rule:
+            rule = none
+        elif gold in rule:
+            rule = gold
+        else:        
+            rule = [r[2:].replace(" bags", "").replace(" bag", "") for r in rule.replace("\n", "").replace(".", "").split(", ")]
+        
+        rules[bag] = rule
+
+def isGold(bag):
+    if rules[bag] == gold:
+        return True
+    elif rules[bag] == none:
+        return False
+    else:
+        return any([isGold(r) for r in rules[bag]])
+
+
+counter = 0
+
+for r in rules:
+    if isGold(r):
+        counter += 1
+
+print(counter)
---
+++ Day 7/7B.py
@@ -0,0 +1,30 @@
+import os.path
+
+rules = dict()
+
+gold = "shiny gold"
+none = "none"
+
+with open(os.path.join("Day 7", "input.txt")) as f:
+    for line in f:
+        bag, rule = line.split(" bags contain ")
+
+        if "no other" in rule:
+            rule = none
+        else:        
+            rule = [r.replace(" bags", "").replace(" bag", "") for r in rule.replace("\n", "").replace(".", "").split(", ")]
+        
+        rules[bag] = rule
+
+def containedBags(bag):
+    contained = 1
+
+    if rules[bag] is none:
+        return contained
+
+    for b in rules[bag]:
+        contained += (int(b[:1]) * containedBags(b[2:]))
+
+    return contained
+
+print(containedBags(gold) - 1)
